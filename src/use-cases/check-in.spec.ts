cd64c7af4f2c785d8c24b71b876bc0844a4e9971
elias.alexandre <elliasallex@gmail.com>
2023-04-24 07:20:57 -0300

    Check Ins Use-Case

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,24 @@
+import { hash } from "bcryptjs";
+import { beforeEach, describe, expect, it } from "vitest";
+
+import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
+import { CheckInUseCase } from "./check-in";
+
+let checkInsRepository: InMemoryCheckInsRepository;
+let sut: CheckInUseCase;
+
+describe("Check-In Use Case", () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository();
+    sut = new CheckInUseCase(checkInsRepository);
+  });
+
+  it("should be able to create check-in", async () => {
+    const { checkIn } = await sut.execute({
+      userId: "user-1",
+      gymId: "gym-1",
+    });
+
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
+});
