aaaf1508b06111ca5f2349e2396da357d58b98bc
elias.alexandre <elliasallex@gmail.com>
2023-09-11 15:06:18 -0300

    validate check-in based on its creation time

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,7 +1,9 @@
 import { CheckIn } from "@prisma/client";
+import dayjs from "dayjs";
 
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { LateCheckInValidationError } from "./errors/late-ckeck-in-validation-error";
 
 interface ValidateCheckInUseCaseRequest {
   checkInId: string;
@@ -20,6 +22,14 @@ export class ValidateCheckInUseCase {
     const checkIn = await this.checkInsRepository.findById(checkInId);
 
     if (!checkIn) throw new ResourceNotFoundError();
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      "minutes"
+    );
+
+    if (distanceInMinutesFromCheckInCreation > 20)
+      throw new LateCheckInValidationError();
 
     checkIn.validated_at = new Date();
     await this.checkInsRepository.save(checkIn);
