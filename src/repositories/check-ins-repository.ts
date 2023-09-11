5eaf0808e14174a6e1c3aa1c385615c6c8a5f940
elias.alexandre <elliasallex@gmail.com>
2023-09-11 14:35:15 -0300

    validate check-in use-case

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,7 +1,9 @@
 import { CheckIn, Prisma } from "@prisma/client";
 
 export interface CheckInsRepository {
+  save(checkIn: CheckIn): Promise<CheckIn>;
   create(checkIn: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
+  findById(checkInId: string): Promise<CheckIn | null>;
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
   countByUserId(userId: string): Promise<number>;
--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -20,7 +20,25 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkIn;
   }
 
-  async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id);
+
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn;
+    }
+
+    return checkIn;
+  }
+
+  async findById(id: string): Promise<CheckIn | null> {
+    const checkIn = this.items.find((item) => item.id === id);
+
+    if (!checkIn) return null;
+
+    return checkIn;
+  }
+
+  async findManyByUserId(userId: string, page: number) {
     return this.items
       .filter((item) => item.user_id === userId)
       .slice((page - 1) * 20, page * 20);
