6cc12656c5c1c89635b07e4a6c4cdc90f1c53a0c
elias.alexandre <elliasallex@gmail.com>
2023-09-12 13:27:24 -0300

    prisma gyms repository

--- src/repositories/prisma/prisma-gyms-repository.ts
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -1,23 +1,38 @@
 import { Gym, Prisma } from "@prisma/client";
+
 import { GymsRepository } from "../gyms-repository";
 import { prisma } from "@/lib/prisma";
+import { FindManyNearbyDTO } from "@/dtos/GymsDTO";
 
 export class PrismaGymsRepository implements GymsRepository {
-  async create(data: Prisma.GymCreateInput): Promise<Gym> {
-    const gym = await prisma.gym.create({
+  async create(data: Prisma.GymCreateInput) {
+    return await prisma.gym.create({
       data,
     });
-
-    return gym;
   }
 
-  async searchMany(query: string, page: number) {}
+  async searchMany(query: string, page: number) {
+    return await prisma.gym.findMany({
+      where: {
+        title: { contains: query },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    });
+  }
 
-  async findById(id: string): Promise<Gym | null> {
-    const gym = await prisma.gym.findUnique({
+  async findManyNearby({ latitude, longitude }: FindManyNearbyDTO) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+        WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( logitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `;
+
+    return gyms;
+  }
+
+  async findById(id: string) {
+    return await prisma.gym.findUnique({
       where: { id },
     });
-
-    return gym;
   }
 }
